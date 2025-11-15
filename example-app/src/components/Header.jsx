5f481e45c3d87990f6593a892b32ef51425d19c7
Ehsan2456473 <2456473@brunel.ac.uk>
2025-11-15 19:44:26 +0000

    extar before answrs come

---
+++ example-app/src/components/Header.jsx
@@ -0,0 +1,23 @@
+// src/components/Header.jsx
+export default function Header() {
+    return (
+        <header
+            style={{
+                textAlign: "center",
+                margin: "auto",
+                width: "100%",
+            }}
+        >
+            <img
+                src="gRPq8b.jpg"
+                alt="Header image"
+                style={{
+                    height: "200px",
+                    width: "100%",
+                    objectFit: "contain",
+                    objectPosition: "center",
+                }}
+            />
+        </header>
+    );
+}
